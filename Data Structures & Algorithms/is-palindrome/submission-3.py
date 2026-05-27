e67d15b6b387846b9d36c4dd376eb4205f1545e8
Argonaut <gaming.genos1729@gmail.com>
2026-05-27 16:08:49 +0530

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,12 @@
+import re
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = re.sub(r"[^a-z0-9]", '', s.lower())
+        i, j = 0, len(s)-1
+        while i<=j:
+            if s[i]!=s[j]:
+                return False
+            i+=1
+            j-=1
+        return True
